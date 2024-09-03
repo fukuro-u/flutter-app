7f297369db3b1ee54d457885080a7a1193fd0f08
fukuro-u <109228464+fukuro-u@users.noreply.github.com>
2024-09-03 18:08:43 +0700

    Update main_header.dart

--- lib/widgets/mainheader/main_header.dart
+++ lib/widgets/mainheader/main_header.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:movie_catalogue/widgets/mainheader/profile_section.dart';
-import 'package:movie_catalogue/widgets/mainheader/search_bar.dart';
+import 'package:movie_catalogue/widgets/mainheader/search_bar.dart'as custom_search;
 
 class MainHeader extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Row(
       children: const [
-        SearchBar(),
+        custom_search.SearchBar(),
         ProfileSection()
       ],
     );
